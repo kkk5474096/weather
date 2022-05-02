48e78b318e21bb3938ec4033ee0ffa564103d44e
Leekangmin <kkk5474096@naver.com>
2022-05-02 22:35:30 +0900

    #5 [feature] ResponseWeather

--- data/src/main/java/com/backpac/weather/data/response/ResponseWeather.kt
+++ data/src/main/java/com/backpac/weather/data/response/ResponseWeather.kt
@@ -1,5 +1,20 @@
 package com.backpac.weather.data.response
 
+import com.google.gson.annotations.SerializedName
+
 data class ResponseWeather(
+    @SerializedName("consolidated_weather")
+    val consolidated_weather: List<ResponseWeatherDetail>,
     val title: String
-)
+) {
+    data class ResponseWeatherDetail(
+        @SerializedName("weather_state_name")
+        val weatherStateName: String,
+        @SerializedName("weather_state_abbr")
+        val weatherStateAbbr: String,
+        @SerializedName("the_temp")
+        val theTemp: Double,
+        @SerializedName("humidity")
+        val humidity: Int
+    )
+}
